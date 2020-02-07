ec3a4fdc770f45e9b5d207b720f62d25bef9586a
Ioana Medvesan <ioanamedvesan@yahoo.com>
2020-02-07 19:43:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function keepGoing (array, changeValue, skipValue) {
 function fndBy (array, findFn) {
   for (let i = 0; i < array.length; i++) {
     if (findFn(array[i])) {
-      return array[i];
+      return findFn(array[i]);
     }
   }
   return null;
