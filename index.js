a359d1090f2d9d92a3228c08cf807f259894087d
Ioana Medvesan <ioanamedvesan@yahoo.com>
2020-02-07 19:55:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function keepGoing (array, changeValue, skipValue) {
 }
 
 
-function fndBy (array, findFn) {
+function findBy (array, findFn) {
   for (let i = 0; i < array.length; i++) {
     if (findFn(array[i])) {
       return findFn(array[i]);
